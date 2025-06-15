079d1292d438e508aa73cd346c9a2819db382cfc
armin <armin.arndt@googlemail.com>
2025-06-15 18:45:25 +0200

    fix header title

--- app/(user)/components/ui/header/Logo.tsx
+++ app/(user)/components/ui/header/Logo.tsx
@@ -35,10 +35,10 @@ export default function Logo (props: LogoProps) {
 
     return (
         <div className={
-            "fixed flex bg-primary/30 backdrop-blur shadow-[0_0px_10px_rgba(0,0,0,0.25)] md:left-auto right-0 h-[60px] ml-2 p-auto align-middle md:whitespace-nowrap text-3xl/[1] md:text-5xl/[1.6] font-bold transition-all rounded-l-[50px]  px-4 "
+            "fixed flex bg-primary/30 backdrop-blur shadow-[0_0px_10px_rgba(0,0,0,0.25)] md:left-auto right-0 h-[60px] mx-2 p-auto align-middle md:whitespace-nowrap text-3xl/[1] md:text-5xl/[1.6] font-bold transition-all rounded-l-[50px]  px-4 "
             + (scrollDirection === "down" ? "w-0 pr-0 pl-[110px] text-transparent" : " md:w-auto pr-[110px]")
             }>
-              <Link href="/" className="no-underline ">
+              <Link href="/" className="m-auto no-underline">
                 {props.title}
               </Link>
         </div>
