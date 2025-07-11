e0343f24f820297e7b77288823743cf8a70466b6
armin <armin.arndt@googlemail.com>
2025-07-11 15:14:07 +0200

    Order of project list with 3 columns

--- app/(user)/components/contentBlock/projectList.tsx
+++ app/(user)/components/contentBlock/projectList.tsx
@@ -1,64 +1,73 @@
-import { Image } from "next-sanity/image";
-
-import { urlForImage } from "@/sanity/lib/utils";
-
 import { sanityFetch } from "@/sanity/lib/fetch";
 import { projectListQuery } from "@/sanity/lib/queries";
 import Link from "next/link";
 import PreviewImage from "./previewImage";
 
 
-interface ProjectListProps { 
-    _key: string;
-    _type: string; 
-    showAllProjects: boolean;
-    params: { slug: string }
-}
-
 export default async function ProjectList(props: any) {
     const params = props.params;
 
-    const [projects] = await Promise.all([
-      sanityFetch({ query: projectListQuery, params })
+    const [originalProjects] = await Promise.all([
+        sanityFetch({ query: projectListQuery, params })
     ]);
+
+    function reorderColumns(arr: Array<any>, numColumns: number) {
+        const result = [];
+        for (let col = 0; col < numColumns; col++) {
+            for (let i = col; i < arr.length; i += numColumns) {
+                result.push(arr[i]);
+            }
+        }
+        return result;
+    }
+
+    const reorderProjects = reorderColumns(originalProjects, 3);
+
 
     return (
         <div className="w-full pt-10 sm:p-10 sm:pt-20">
-            {/* <p>{JSON.stringify(projects)}</p> */}
-            <div className="columns-1 sm:columns-2 md:columns-3 gap-10 space-y-4">
-                { projects.map((project: any, index: number) => {
-                    return (
-                        <div key={index} className="break-inside-avoid pb-20">
-                                <div className="transition-shadow duration-200 group-hover:shadow-lg sm:mx-0">
-                                <Link href={project?.slug ?? "/"}>
-                                    <PreviewImage image={project.previewImage}  />
-                                    </Link>
-                                </div>
-                                <div className="pt-2 px-4 bottom-0 left-0 right-0">
-                                    <div className="text-sm">{project.year}</div>
-                                    
-                                    <h3 className="px-0 py-0 m-0 ">
-                                        <Link href={project?.slug ?? "/"} className="no-underline">
-                                            {project.title}
-                                        </Link>
-                                    </h3>
-
-                                    <div className="text-sm" style={{overflowWrap: 'anywhere'}} >
-                                        {project.category?.map((category: any, i: number) => (
-                                            <span key={`category-${category?.slug || i}`} className="text-nowrap mr-4">
-                                                {category?.title}
-                                            </span>
-                                        ))}
-                                    </div>
-                                    {/* <div className="px-4">
-                                        <div className="underline float-right text-sm">⇢ read more</div>
-                                    </div> */}
-                                </div>
-                            
-                        </div>
-                    )
-                })}
+            <div className="hidden sm:block columns-3 gap-10 space-y-4">
+                { mapProjects(reorderProjects) }
+            </div>
+            <div className="sm:hidden columns-1 gap-10 space-y-4">
+                { mapProjects(originalProjects) }
             </div>
         </div>
     )
+}
+
+
+const mapProjects = (projects: Array<any>) => {
+    return projects.map((project: any, index: number) => {
+        return (
+            <div key={index} className="break-inside-avoid pb-20">
+                <div className="transition-shadow duration-200 group-hover:shadow-lg sm:mx-0">
+                    <Link href={project?.slug ?? "/"}>
+                        <PreviewImage image={project.previewImage} />
+                    </Link>
+                </div>
+                <div className="pt-2 px-4 bottom-0 left-0 right-0">
+
+                    <div className="text-sm">
+                        {project.year}
+                    </div>
+
+                    <h3 className="px-0 py-0 m-0 ">
+                        <Link href={project?.slug ?? "/"} className="no-underline">
+                            {project.title}
+                        </Link>
+                    </h3>
+
+                    <div className="text-sm" style={{ overflowWrap: 'anywhere' }} >
+                        {project.category?.map((category: any, i: number) => (
+                            <span key={`category-${category?.slug || i}`} className="text-nowrap mr-4">
+                                {category?.title}
+                            </span>
+                        ))}
+                    </div>
+
+                </div>
+            </div>
+        )
+    })
 }
